48397fb6ed5f113a05e929491e77c2d341fcc4a4
YuFengjie97 <2457522528@qq.com>
2022-12-06 10:22:10 +0800

    feat: 环境变量

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ThreeTemplate-webpack
 hi,this is a easy demo for three.js to use webpack build,just for learn and pratice
+
+## preview
+
+[preview](https://yufengjie97.github.io/ThreeTemplate-webpack/)
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,57 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {resolve} = path
+const { resolve } = path
 
-module.exports = {
-  mode: 'development',
-  entry: './index.ts',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+module.exports = (env) => {
+  console.log('######is Production?########', env.production)
+  const production = env.production
+  return {
+    mode: production ? 'production' : 'development',
+    entry: './index.ts',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
+      extensions: ['.tsx', '.ts', '.js'],
     },
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: resolve(__dirname, 'docs'),
-    clean: true,
-    publicPath: '/',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: path.resolve(__dirname, 'docs'),
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'ThreeTemplate-webpack',
-      template: resolve(__dirname, 'index.html'),
-      favicon: resolve(__dirname, 'src/assets/icon.svg'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.less$/i,
-        use: ['style-loader', 'css-loader', 'less-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    output: {
+      filename: 'bundle.js',
+      path: resolve(__dirname, 'docs'),
+      clean: true,
+      publicPath: production ? '/ThreeTemplate-webpack/' : '/',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      static: path.resolve(__dirname, './docs'),
+      hot: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'ThreeTemplate-webpack',
+        template: resolve(__dirname, 'index.html'),
+        favicon: resolve(__dirname, 'src/assets/icon.svg'),
+      }),
     ],
-  },
-  // 多入口文件的配置
-  // optimization: {
-  //   runtimeChunk: 'single',
-  // },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.less$/i,
+          use: ['style-loader', 'css-loader', 'less-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  }
 }
